1a12a2cacec2b82475daddb14e59e75222c99a14
francesco.loreti <francesco.loreti@isprambiente.it>
2025-06-10 20:10:20 +0200

    Aggiunti riferimenti nei file compilati

--- compile.js
+++ compile.js
@@ -3,10 +3,15 @@ const fs = require('fs');
 
 // Legge il file package.json
 const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const description = pkg.description; // Estrae la descrizione
 const version = pkg.version; // Estrae la versione
+const license = pkg.license; // Estrae la licenza
+const homepage = pkg.homepage; // Estrae la homepage
+const authors = pkg.author; // Estrae l'autore
 
 // Funzione per aggiungere il commento al CSS
-const addVersionComment = (css) => `/* Tema Bulma per ISPRA - Versione: ${version} */\n` + css;
+const addVersionComment = (css) => `/*!\n  ${description} v${version}\n  ${authors}\n  ${license}\n  ${homepage}\n*/\n\n` + css;
+const addVersionCommentMin = (css) => `/*! ${description} v${version} | ${authors} | ${license} | ${homepage} */` + css;
 
 // Standard compilation
 const result = sass.compile('index.scss', {
@@ -19,6 +24,6 @@ const compressedResult = sass.compile('index.scss', {
   loadPaths: ['node_modules'],
   style: 'compressed'
 });
-fs.writeFileSync('dist/bulma-theme-ispra.min.css', addVersionComment(compressedResult.css));
+fs.writeFileSync('dist/bulma-theme-ispra.min.css', addVersionCommentMin(compressedResult.css));
 
 console.log('CSS compiled successfully!');
